859bb45421c4558d5ced77850b070251a5dd03f2
ozgurdenizyusuf <yusuftuncdemir@ogr.iu.edu.tr>
2023-09-15 09:01:50 +0300

    DeepThought
    DeepThought

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Ask me a question:");
+while (true)
+{
+    string soru = Console.ReadLine();
+    string cevap = "The answer is: 42";
+    Console.WriteLine(cevap);
+    Console.WriteLine("Ask me another question:");
+}
